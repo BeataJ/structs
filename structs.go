2a23a7bbe981ac93188c9d3710861afa61c5f749
Beata <beata@jasinski.com>
2024-03-21 12:32:33 -0700

    creat struct method

--- structs.go
+++ structs.go
@@ -12,6 +12,11 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	// ....
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -28,13 +33,7 @@ func main() {
 
 	// ... do something awesome with that gathered data!
 
-	// fmt.Println(firstName, lastName, birthdate)
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) {
-	// ....
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
